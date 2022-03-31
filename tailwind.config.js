93ea8c1baa3e6ad7ed24f1388b12b9681ec3a233
hibaaa <93978029+joumarhiba@users.noreply.github.com>
2022-04-01 01:44:51 +0200

    frontend

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["/src/components/"],
+    theme: {
+      container: {
+        center:true,
+        padding:'2rem'
+      },
+    },
+    plugins: [
+      
+    ],
+  }
